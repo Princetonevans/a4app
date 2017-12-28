6dbbf79ca19ded92b0f953258269a5373c1eef71
Princeton <Princetonevans@yahoo.com>
2017-12-28 11:09:34 -0600

    added model to user component

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  name:string;
+  age:number;
+  email:string;
+  address:Address;
+
+
+  constructor() { console.log('constructor ran..'); }
+
+
+  ngOnInit() {
+    console.log('ngOnInit ran');
+
+    this.name = 'John Doe';
+    this.age = 30;
+    this.address = {
+      street: '50 Main st',
+      city: 'Boston',
+      state: 'MA'
+    }
+
+  }
+
+}
+
+interface Address {
+    street:string,
+    city:string,
+    state:string
+}
